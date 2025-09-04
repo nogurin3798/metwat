ed41dc612a517a5958df4ef0eea74ef98475b9e6
nogurin3798 <51079421+nogurin3798@users.noreply.github.com>
2025-09-04 11:36:33 +0900

    Add files via upload
    うpぉあÐv4.4

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-const CACHE='metwat-pwa-v42';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-192.png','./icons/maskable-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));self.clients.claim();});
-self.addEventListener('fetch',e=>{const url=new URL(e.request.url);
-  if(url.pathname.endsWith('/app.js')){e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>caches.match(e.request)));}
-  else{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{caches.open(CACHE).then(ca=>ca.put(e.request,r.clone()));return r;})));}});
+const CACHE='metwat-pwa-v44';self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
