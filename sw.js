e302907a4f9162c9fae29622073fe3aeab33f083
nogurin3798 <51079421+nogurin3798@users.noreply.github.com>
2025-09-15 08:46:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,5 @@
-const CACHE='metwat-pwa-v46-explain-src-1';
+const CACHE='metwat-pwa-v47';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim();});
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.pathname.endsWith('/app.js')){
-    e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone())); return r;}).catch(()=>caches.match(e.request)));
-  }else{
-    e.respondWith(caches.match(e.request).then(c=> c || fetch(e.request).then(r=>{caches.open(CACHE).then(C=>C.put(e.request,r.clone())); return r;})));
-  }
-});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone())); return res;})));});
