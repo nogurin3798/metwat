0b2acb2662277abdc9bf095eb484c12b477c9fab
nogurin3798 <51079421+nogurin3798@users.noreply.github.com>
2025-09-16 19:13:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE='metwat-pwa-v48-complete-1';
+const CACHE='metwat-pwa-v48-autonow-1';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim();});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(c=> c || fetch(e.request).then(r=>{caches.open(CACHE).then(C=>C.put(e.request,r.clone())); return r;})));
-});
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(c=> c || fetch(e.request).then(r=>{caches.open(CACHE).then(C=>C.put(e.request,r.clone())); return r;}))); });
