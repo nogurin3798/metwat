5f1839ef299365f68ef75a25f58d354077a37941
nogurin3798 <51079421+nogurin3798@users.noreply.github.com>
2025-09-21 14:58:59 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='metwat-pwa-v48-autonow-1';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim();});
-self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(c=> c || fetch(e.request).then(r=>{caches.open(CACHE).then(C=>C.put(e.request,r.clone())); return r;}))); });
